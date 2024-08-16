16811759c9df0e02f9b8c864a93cd87b60fff468
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-08-16 23:54:35 +0530

    day47

---
+++ Day47/max.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+
+int maxDistance(std::vector<std::vector<int>>& arrays) {
+    int min_val = arrays[0].front();
+    int max_val = arrays[0].back();
+    int max_distance = 0;
+
+    for (int i = 1; i < arrays.size(); ++i) {
+         max_distance = std::max(max_distance, std::abs(arrays[i].back() - min_val));
+        max_distance = std::max(max_distance, std::abs(max_val - arrays[i].front()));
+        min_val = std::min(min_val, arrays[i].front());
+        max_val = std::max(max_val, arrays[i].back());
+    }
+
+    return max_distance;
+}
+};
